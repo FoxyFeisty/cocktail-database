540f4011b3ad1b76b256d204c36e769bd4ab78fe
FoxyFeisty <36619363+FoxyFeisty@users.noreply.github.com>
2018-07-19 08:37:47 +0200

    Add files via upload

---
+++ admin/after_update.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet"> 
+    <link rel="stylesheet" href="../cocktail.css">
+    <title>Eintrag aktualisiert</title>
+  </head>
+  <body>
+    <div id="uploadInfo">
+      <h1 class="mainHL">Änderung erfolgt</h1>
+      <div>Sie haben die Datenbank erfolgreich aktualisiert.</div>
+      <div class="moveIn">
+      <button class="button" id="btnToForm" type="button">Neuer Cocktail-Eintrag</button>
+      <button class="button" id="btnToList" type="button">Zurück zur Übersicht</button></div>
+   </div>
+    <script src="testform.js" type="text/javascript"></script>
+  </body>
+</html>
+
